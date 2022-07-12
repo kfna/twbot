78e4fe70e85755c378467673274789c3ad83650e
Luis Cruz <luis@kafeina.com>
2022-07-11 22:09:33 -0500

    update

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ require('dotenv').config();
 
 console.log("Done! 122");
 
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(80); //the server object listens on port 80
+
 var T = new Twit({
   consumer_key:         process.env.CONSUMER_KEY,
   consumer_secret:       process.env.CONSUMER_SECRET,
